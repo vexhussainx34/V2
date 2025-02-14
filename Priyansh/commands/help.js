0e2bf15fc2240a31c80b3779b42d5ba39b6c4eeb
vexhussain56 <vexhussainx@yahoo.com>
2025-02-14 08:44:55 +0000

    Update help.js

--- Priyansh/commands/help.js
+++ Priyansh/commands/help.js
@@ -73,7 +73,7 @@ module.exports. run = function({ api, event, args, getText }) {
     for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;
     
     
-    const siu = `Command list 📄\nMade by Prîyánsh Rajput 🥀\nFor More Information type /help (command name) ✨\n󰂆 󰟯 󰟰 󰟷 󰟺 󰟵 󰟫`;
+    const siu = `Command list 📄\nMade by 𝗠𝗥_𝗛𝗨𝗦𝗦𝗔𝗜𝗡 🥀\nFor More Information type /help (command name) ✨\n𝗠𝗥_𝗛𝗨𝗦𝗦𝗔𝗜𝗡`;
     
  const text = `\nPage (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})\n`;
  
